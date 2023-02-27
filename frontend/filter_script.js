92050b08f5742fe970b48c563285769d8ea7b05d
charlestian23 <charles.tian23@gmail.com>
2023-02-27 17:37:44 -0500

    Saving initial progress on trying to align the label columns

--- frontend/filter_script.js
+++ frontend/filter_script.js
@@ -1,7 +1,7 @@
 function toggleFilterWindow() {
     const filterWindow = document.getElementById("filter_content");
     if (window.getComputedStyle(filterWindow).display === "none") {
-        filterWindow.style.display = "block";
+        filterWindow.style.display = "flex";
     }
     else {
         filterWindow.style.display = "none";
