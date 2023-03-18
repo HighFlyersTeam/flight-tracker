aee53badbbd93ca5390252c6face0fd7283aee78
charlestian23 <charles.tian23@gmail.com>
2023-03-18 15:39:42 -0400

    Added prototype for dynamic start selection

--- frontend/filter_script.js
+++ frontend/filter_script.js
@@ -1,3 +1,4 @@
+// Toggles the filter window
 function toggleFilterWindow() {
     const filterWindow = document.getElementById("filter_content");
     if (window.getComputedStyle(filterWindow).display === "none") {
@@ -7,3 +8,16 @@ function toggleFilterWindow() {
         filterWindow.style.display = "none";
     }
 }
+
+// Changes selection for each item in the dropdown menu
+$(document).on("change", "#start_filter", function() {
+    var target = $(this).data('target');
+    var show = $("option:selected", this).data('show');
+    $(target).children().addClass('hide');
+    $(show).removeClass('hide');
+});
+
+// Displays the selection for the first item in the dropdown menu
+$(function(){
+    $('#start_filter').trigger('change');
+});
