4701ba5db5fdd7fae8f8804ba61406c9d692c116
charlestian23 <charles.tian23@gmail.com>
2023-03-18 17:16:05 -0400

    Added advanced controls
    Added option to find added flights and removed flights between a second time period

--- frontend/filter_script.js
+++ frontend/filter_script.js
@@ -17,8 +17,32 @@ $(document).on("change", "#start_filter, #end_filter", function() {
     $(show).removeClass('hide');
 });
 
-// Displays the selection for the first item in the dropdown menu
+// Toggles the time selection for the advanced controls
+function advanced_controls_time_display() {
+    const start_time = document.getElementById("secondary_start_time");
+    const end_time = document.getElementById("secondary_end_time");
+    console.log(start_time.style.display);
+    if (start_time.style.display === "none") {
+        console.log("true");
+        start_time.style.removeProperty("display");
+        end_time.style.removeProperty("display");
+    }
+    else {
+        console.log("false");
+        start_time.style.display = "none";
+        end_time.style.display = "none";
+    }
+}
+
+// Triggers on start of the webpage creation
 $(function(){
+    // Displays the selection for the first item in the dropdown menu
     $('#start_filter').trigger('change');
     $('#end_filter').trigger('change');
+
+    // Hide the time selection for the advanced controls
+    const start_time = document.getElementById("secondary_start_time");
+    const end_time = document.getElementById("secondary_end_time");
+    start_time.style.display = "none";
+    end_time.style.display = "none";
 });
