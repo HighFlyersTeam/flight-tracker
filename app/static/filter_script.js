f11b7d67ad1a754bead283d7fa1947d0421c68f3
Ivan Ho <41582274+Corppet@users.noreply.github.com>
2023-03-23 23:11:15 -0400

    Updated Help window body

--- app/static/filter_script.js
+++ app/static/filter_script.js
@@ -1,8 +1,10 @@
 // Toggles the filter window
 function toggleFilterWindow() {
     const filterWindow = document.getElementById("filter_content");
+    const helpWindow = document.getElementById("help_content");
     if (window.getComputedStyle(filterWindow).display === "none") {
         filterWindow.style.display = "flex";
+        helpWindow.style.display = "none";
     }
     else {
         filterWindow.style.display = "none";
